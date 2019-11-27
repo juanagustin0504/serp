e7c8b71ef7dd59bae03c6fad93a5da94fd1a9a0d
juanagustin0504 <juanagustin@naver.com>
2019-11-27 13:08:46 +0900

    Added js->native, native->js

--- serp-sample/ViewController.swift
+++ serp-sample/ViewController.swift
@@ -9,33 +9,77 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
+    
+    @IBOutlet var containerView: UIView!
+    
     
     var webView: WKWebView!
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
     override func loadView() {
         super.loadView()
         
-        webView = WKWebView(frame: self.view.frame)
+        let contentController = WKUserContentController()
+        let config = WKWebViewConfiguration()
+        
+        // native -> js call (문서 시작시에만 가능한, 환경설정으로 사용함), source부분에 함수 대신 HTML직접 사용 가능 //
+        let userScript = WKUserScript(source: "redHeader()", injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        contentController.addUserScript(userScript)
+        
+        // js -> native call : name의 값을 지정하여, js에서 webkit.massageHandlers.NAME.postMessage("");와 연동되는 것, userContentController 함수에서 처리한다. //
+        contentController.add(self, name: "callbackHandler")
+        
+        config.userContentController = contentController
+        
+        
+        webView = WKWebView(frame: self.containerView.frame, configuration: config)
         webView.uiDelegate = self
         webView.navigationDelegate = self
         
-        self.view = self.webView!
+//        self.view = self.webView!
+        self.view.addSubview(webView)
+        print("loadView")
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let lnk = "https://serpadmin.appplay.co.kr/pfmc_0001_00.act"
+//        let lnk = "https://serpadmin.appplay.co.kr/pfmc_0001_00.act"
+//
+        let lnk = "file:///Users/webcash/Assignment/serp-sample/serp-sample/test.html"
         let url = URL(string: lnk)
         let request = URLRequest(url: url!)
         
         
         webView.load(request) // 웹뷰 띄우기 //
+        print("viewDidLoad")
         
     }
 
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let otherAction = UIAlertAction(title: "OK", style: .default, handler: {action in completionHandler()})
+        alert.addAction(otherAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    // js -> native call
+    @available(iOS 8.0, *)
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "callbackHandler" {
+            print(message.body)
+            abc()
+        }
+    }
+    
+    func abc() {
+        print("abc call")
+    }
 
 }
 
