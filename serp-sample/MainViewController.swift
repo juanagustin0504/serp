a7c749b2bc9757db2f41667e6db96982ff2aef94
juanagustin0504 <juanagustin@naver.com>
2019-12-03 11:15:09 +0900

    Added decidePolicyFor navigationAction. but, crash.

--- serp-sample/MainViewController.swift
+++ serp-sample/MainViewController.swift
@@ -15,7 +15,6 @@ class MainViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, 
     @IBOutlet weak var containerView: UIView!
     
     var webView: WKWebView!
-    var popupView: WKWebView!
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
     let contentController = WKUserContentController()
@@ -37,11 +36,11 @@ class MainViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, 
         config.userContentController = contentController
         
         
-        webView = WKWebView(frame: self.containerView.frame, configuration: config)
-        webView.uiDelegate = self
-        webView.navigationDelegate = self
-        webView.allowsBackForwardNavigationGestures = true
-        webView.allowsLinkPreview = false
+        self.webView = WKWebView(frame: self.containerView.frame, configuration: config)
+        self.webView.uiDelegate = self
+        self.webView.navigationDelegate = self
+//        webView.allowsBackForwardNavigationGestures = true
+//        webView.allowsLinkPreview = false
         
         //        self.view = self.webView!
         self.view.addSubview(webView)
@@ -55,72 +54,31 @@ class MainViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, 
         let url = URL(string: lnk)
         let request = URLRequest(url: url!)
         
-        
-        
         webView.load(request) // 웹뷰 띄우기 //
         
     }
     
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        guard let url = navigationAction.request.url else {
+            decisionHandler(.cancel)
+            return
+        }
+        if url.host == "serpadmin.appplay.co.kr" {
+            print("serp")
+        } else {
+            print("another")
+        }
         
     }
     
     func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
         let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        //        print(message)
         
         alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
             completionHandler()
         }))
         
         self.present(alertController, animated: true, completion: nil)
-        
-        // JavaScript Alert으로 전송되는 값 읽어서 iWebAction: 이후 부터 값을 읽어온 다음 처리 //
-        // {
-        //      "_action_code":"popup_webview",
-        //      "_action_data":{
-        //      "_url":"test_pfmc.act?LAST_LOGIN_DT=20191128&USE_INIT_ID=UTLZ_1711101710714...",
-        //      "_type":"get"
-        // }
-        
-        //        let tmp = message.components(separatedBy: [" ", "\"", ":", "{", "}"])
-        //
-        //        print(tmp)
-        
-        
-        //        let encoder = JSONEncoder()
-        //        encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
-        //        let moon = Person(name: "Moon", age: 19)
-        //        let jsonData = try? encoder.encode(moon)
-        //
-        //        if let jsonData = jsonData, let jsonString = String(data: jsonData, encoding: .utf8) {
-        //            print(jsonString)
-        //
-        //            let decoder = JSONDecoder()
-        //            let data = jsonString.data(using: .utf8)
-        //            if let data = data, let myPerson = try? decoder.decode(Person.self, from: data) {
-        //                print(myPerson.name)
-        //                print(myPerson.age)
-        //            }
-        //        }
-        
-        //        let encoder = JSONEncoder()
-        //        let test = iWebViewAction(_action_code: "popup_webview", _url: "test_pfmc.act?LAST_LOGIN_DT=20191129&USE_INTT_ID=UTLZ_1711131611099&USE_INTT_NM=%EA%B4%80%EC%84%B8%EB%B2%95%EC%9D%B8%EB%8D%94%EB%B8%94%EC%9C%A0&ADDR=%EC%84%9C%EC%9A%B8%EC%8B%9C%20%EA%B0%95%EB%82%A8%EA%B5%AC%20%EC%96%B8%EC%A3%BC%EB%A1%9C%20714&DTL_ADDR=&ZPCD=&BIZ_NO=&SUMR_YM=2019%EB%85%84%2010%EC%9B%94&LOGN_PCNT_01=21&LOGN_PCNT_02=51&LOGN_PCNT_03=0&SALE_PCNT_01=0&SALE_PCNT_02=0&TRNS_PCNT_01=0&TRNS_PCNT_02=125&TRNS_PCNT_03=0&CTNT_PCNT_01=0&CTNT_PCNT_02=86&CTNT_PCNT_03=0&CTNT_PCNT_04=0&SMRT_PCNT_01=0&SMRT_PCNT_02=0&SMRT_PCNT_03=0&SMRT_PCNT_04=0&SMRT_PCNT_05=0&SMRT_PCNT_06=0&SMRT_PCNT_07=0&RPRT_PCNT_01=0&RPRT_PCNT_02=0&RPRT_PCNT_03=0", _type: "get")
-        //        let jsonData = try? encoder.encode(test)
-        //
-        //        if let jsonData = jsonData, let jsonString = String(data: jsonData, encoding: .utf8) {
-        //            print(jsonString)
-        //
-        //            let decoder = JSONDecoder()
-        //            let data = jsonString.data(using: .utf8)
-        //            if let data = data, let iMessage = try? decoder.decode(iWebViewAction.self, from: data) {
-        //                print(iMessage._action_code)
-        //
-        //                print(iMessage._url)
-        //                print(iMessage._type)
-        //
-        //            }
-        //        }
         
         if message.lowercased().contains("iwebaction:") {
             var actionDic: [String: Any]?
@@ -135,7 +93,6 @@ class MainViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, 
                         if actionCode == "popup_webview" {
                             let lnk = (actionData["_url"] as? String)!
                             let type = actionData["_type"] as! String
-                            //                            let type = "post"
                             if type.elementsEqual("get") {
                                 // get 처리
                                 popupWebViewByGet(lnk: lnk)
@@ -152,7 +109,6 @@ class MainViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, 
                         if actionCode == "close_webview" {
                             closeWebview()
                         }
-                        print(actionCode)
                     }
                 }
             }
@@ -161,13 +117,11 @@ class MainViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, 
     }
     
     func popupWebViewByGet(lnk: String) {
-        print("GET")
+        
         let url = URL(string: "https://serpadmin.appplay.co.kr/pfmc_0001_00.act/" + lnk)
-        let domain = url?.host
-        print(domain!)
         let request = URLRequest(url: url!)
-        print("popup! \(request)")
-        webView.load(request)
+        self.webView.load(request)
+        
     }
     
     func popupWebViewByPost(lnk: String, actionData: [String:Any]) {
@@ -203,7 +157,6 @@ class MainViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, 
     }
     
     func closeWebview() {
-        print("closeWebview()")
         //        popupView.isHidden = true
         if webView.canGoBack {
             
@@ -217,40 +170,6 @@ class MainViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, 
         //            }
         //            print(error)
         //        })
-    }
-    
-    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
-        print("@")
-        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        
-        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
-            completionHandler(true)
-        }))
-        
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
-            completionHandler(false)
-        }))
-        
-        self.present(alertController, animated: true, completion: nil)
-    }
-    
-    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
-        print("#")
-        let alertController = UIAlertController(title: nil, message: prompt, preferredStyle: .alert)
-        
-        alertController.addTextField { (textField) in
-            textField.text = defaultText
-        }
-        
-        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
-            if let text = alertController.textFields?.first?.text {
-                completionHandler(text)
-            } else {
-                completionHandler(defaultText)
-            }
-            
-            
-        }))
     }
     
     
