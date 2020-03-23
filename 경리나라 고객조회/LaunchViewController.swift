3c732d8081903fe96f6aa4e54971ccae074ba553
juanagustin0504 <juanagustin@naver.com>
2020-03-23 18:34:03 +0900

    - modify LaunchScreenImage

--- 경리나라 고객조회/LaunchViewController.swift
+++ 경리나라 고객조회/LaunchViewController.swift
@@ -12,7 +12,18 @@ import UIKit
  * 앱이 실행될 때 앱 초기 설정 데이터 받아오는 뷰컨
  */
 
-let images = ["intro_bg_1125x2436.png", "intro_bg_1242.png", "intro_bg_1280.png", "intro_bg.png"]
+enum iPhoneDeviceName: String {
+    
+    case iPhone5s       = "iPhone 5s"
+    case iPhone6        = "iPhone 6"
+    case iPhone6p       = "iPhone 6Plus"
+    case iPhone6s       = "iPhone 6s"
+    case iPhone6sp      = "iPhone 6s Plus"
+    case iPhoneX        = "iPhone X"
+    case iPhoneXmax     = "iPhone X Max"
+    case iPhone11ProMax = "iPhone 11 Pro Max"
+}
+
 class LaunchViewController: UIViewController {
 
     
@@ -20,6 +31,21 @@ class LaunchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(UIDevice.current.name)
+        switch UIDevice.current.name {
+        case iPhoneDeviceName.iPhone5s.rawValue:
+            imageView.image = UIImage(named: "intro_bg_5s.png")
+        case iPhoneDeviceName.iPhone6.rawValue, iPhoneDeviceName.iPhone6p.rawValue, iPhoneDeviceName.iPhone6s.rawValue, iPhoneDeviceName.iPhone6sp.rawValue:
+            imageView.image = UIImage(named: "intro_bg_6s.png")
+        case iPhoneDeviceName.iPhoneX.rawValue:
+            imageView.image = UIImage(named: "intro_bg_x.png")
+        case iPhoneDeviceName.iPhoneXmax.rawValue:
+            imageView.image = UIImage(named: "intro_bg_xmax.png")
+        case iPhoneDeviceName.iPhone11ProMax.rawValue:
+            imageView.image = UIImage(named: "intro_bg_11.png")
+        default:
+            imageView.image = UIImage(named: "intro_bg_x.png")
+        }
         
         imageView.frame = self.view.frame
     }
